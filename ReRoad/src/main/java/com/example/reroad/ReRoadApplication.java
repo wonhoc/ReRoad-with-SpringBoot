846451269df7543e3d71beaa1ac67f335a179b6b
cwh <lion946@naver.com>
2021-12-27 18:46:41 +0900

    update

---
+++ ReRoad/src/main/java/com/example/reroad/ReRoadApplication.java
@@ -0,0 +1,13 @@
+package com.example.reroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReRoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReRoadApplication.class, args);
+    }
+
+}
---
+++ ReRoad/src/test/java/com/example/reroad/ReRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

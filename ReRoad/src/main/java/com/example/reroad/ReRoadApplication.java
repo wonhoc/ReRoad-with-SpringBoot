db8db68d2889658bbfeb4f08c47027b81bf5a844
cwh <lion946@naver.com>
2021-12-28 10:44:40 +0900

    update

--- ReRoad/src/main/java/com/example/reroad/ReRoadApplication.java
+++ ReRoad/src/main/java/com/example/reroad/ReRoadApplication.java
@@ -1,5 +1,4 @@
 package com.example.reroad;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;

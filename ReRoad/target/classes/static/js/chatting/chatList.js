03ae9e536805e383e9cdae6a8278e31b65dcacb4
ksj <ksssj1371@naver.com>
2022-01-18 12:29:54 +0900

    [소켓주소변경과파일수정]

--- ReRoad/target/classes/static/js/chatting/chatList.js
+++ ReRoad/target/classes/static/js/chatting/chatList.js
@@ -1,15 +1,15 @@
 function fetchAll() {
-    const chaturl = 'http://localhost:8080';
+    const chaturl = 'ec2-54-180-31-9.ap-northeast-2.compute.amazonaws.com';
     const myname = $('#myname').val();
     $('#refresh').hide();
-    $.get(chaturl + "/fetchAllUsers", function (response) {
+    $.get("/fetchAllUsers", function (response) {
         let users = response;
         let usersTemplateHTML = "<button id=\"hideuser\" onclick=\"hide()\">유저 목록 숨기기</button>";
 
        if(users.length == 0) {
            usersTemplateHTML = usersTemplateHTML  +
                '<li class="clearfix">' +
-               '                  <div id="nobody">이럴수가 ! 현재 채팅방에 아무도 없네요 :-(</div>\n' +
+               '                  <div id="nobody"><h5 id="noone">이럴수가 ! 현재 채팅방에 아무도 없네요 :-(</h5></div>\n' +
                '            </li>';
            $('#usersList').html(usersTemplateHTML);
        }else {

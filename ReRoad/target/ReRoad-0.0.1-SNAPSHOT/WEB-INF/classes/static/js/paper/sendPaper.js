8cfebbd9994a5ab5c04aab3a2ebf67a62a8d79b4
cwh <lion946@naver.com>
2022-01-18 13:05:02 +0900

    update

--- ReRoad/target/ReRoad-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/paper/sendPaper.js
+++ ReRoad/target/ReRoad-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/paper/sendPaper.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    //전체 삭제 클릭 시
+    $('#allChecked').on('click',function() {
+        // 전체 체크를 할 때
+        if ($('#allChecked').is(':checked')) {
+            //체크를 true로
+            $("input:checkbox[name=removeCheckBox]").prop("checked",true);
+        } else {
+            //체크를 false로
+            $("input:checkbox[name=removeCheckBox]").prop("checked",false);
+        }
+    })
 
 
 })

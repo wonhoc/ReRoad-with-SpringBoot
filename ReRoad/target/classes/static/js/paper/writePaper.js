bfdd8d4af611c5063785536274ee10cbd9403f11
cwh <lion946@naver.com>
2022-01-18 12:46:00 +0900

    update

---
+++ ReRoad/target/classes/static/js/paper/writePaper.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // 쪽지의 글자수 100자 제한
+    $('#sendContent').on('keyup',function() {
+        $('#checkResult').html($(this).val().length);
+
+        if($(this).val().length > 100) {
+            $(this).val($(this).val().substring(0,100));
+            $('#checkResult').html("100");
+        }
+    })
+
+
+
+})
